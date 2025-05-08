835a93fa6808956a473835ed113ca6761bfea557
Arkifane <DUF24001762@heartofyorkshire.ac.uk>
2025-05-08 16:54:31 +0100

    ONE OF THE FINAL COMMITS

--- FMP-Escape Game/Assets/Scripts/WaypointAI.cs
+++ FMP-Escape Game/Assets/Scripts/WaypointAI.cs
@@ -1,46 +1,91 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
 
+using UnityEngine;
+
+using UnityEngine.AI;
+ 
 public class WaypointAI : MonoBehaviour
+
 {
 
     public List<Transform> wayPoint;
 
     NavMeshAgent navMeshAgent;
 
+    FieldOfView fovScript;
+
+    Vector3 targetPosition;
+ 
     public int currentWayPointIndex = 0;
-
-
-
+ 
     // Start is called before the first frame update
+
     void Start()
+
     {
+
         navMeshAgent = GetComponent<NavMeshAgent>();
-    }
 
+        fovScript = transform.GetComponent<FieldOfView>();
+
+        targetPosition = transform.position;
+
+    }
+ 
     // Update is called once per frame
+
     void Update()
+
     {
+
         Walking();
-    }
 
+    }
+ 
     void Walking()
+
     {
 
-        if(wayPoint.Count == 0)
+        if (wayPoint.Count == 0)
+
         {
+
             return;
+
         }
+ 
+        // Check distance to current waypoint
 
         float distanceToWayPoint = Vector3.Distance(wayPoint[currentWayPointIndex].position, transform.position);
+ 
+        // If the waypoint is close, move to the next one
 
-        if(distanceToWayPoint <= 3)
+        if (distanceToWayPoint <= 3)
+
         {
+
             currentWayPointIndex = (currentWayPointIndex + 1) % wayPoint.Count;
+
         }
+ 
+        // Set the destination to the current waypoint
 
         navMeshAgent.SetDestination(wayPoint[currentWayPointIndex].position);
+ 
+        // If the player is within view, set the destination to the player's position
+
+        if (fovScript.canSeePlayer)
+
+        {
+
+            targetPosition = fovScript.playerRef.transform.position;
+
+            navMeshAgent.SetDestination(targetPosition); // Update the destination to the player's position
+
+        }
+
     }
+
 }
+
+ 
